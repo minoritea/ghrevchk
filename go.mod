47591b3774ee73039fd107bd361214e791fefc89
Minori Tokuda <snowcrush@mail.0pt.jp>
2024-04-17 15:38:44 +0900

    use GraphQL instead of REST APIs

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/minoritea/ghrevchk
 
 go 1.22.1
 
-require github.com/google/go-github/v61 v61.0.0
+require github.com/hasura/go-graphql-client v0.12.1
 
-require github.com/google/go-querystring v1.1.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	nhooyr.io/websocket v1.8.10 // indirect
+)
--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
-	"github.com/google/go-github/v61/github"
+	"github.com/hasura/go-graphql-client"
 )
 
 func ownerAndRepo(r string) (string, string) {
@@ -19,6 +20,49 @@ func ownerAndRepo(r string) (string, string) {
 		return ss[1], ss[2]
 	}
 	return "", ""
+}
+
+func fetch(ctx context.Context, owner, repo, user string) error {
+	client := graphql.NewClient("https://api.github.com/graphql", nil).
+		WithRequestModifier(func(r *http.Request) {
+			r.Header.Set("Authorization", "Bearer "+os.Getenv("GITHUB_TOKEN"))
+		})
+	type Query struct {
+		Search struct {
+			Edges []struct {
+				Node struct {
+					PullRequest struct {
+						Title string
+						URL   string
+					} `graphql:"... on PullRequest"`
+				}
+			}
+			PageInfo struct {
+				HasNextPage bool
+				EndCursor   string
+			}
+		} `graphql:"search(query: $query, type: ISSUE, first: 100)"`
+	}
+	result, err := query[Query](ctx, client, map[string]any{
+		"query": fmt.Sprintf(
+			"is:pr is:open repo:%s/%s state:open involves:%s",
+			owner,
+			repo,
+			user,
+		),
+	})
+	if err != nil {
+		return err
+	}
+	for _, edge := range result.Search.Edges {
+		fmt.Printf("%s\n> %s\n\n", edge.Node.PullRequest.Title, edge.Node.PullRequest.URL)
+	}
+	return nil
+}
+
+func query[T any](ctx context.Context, client *graphql.Client, vars map[string]any, options ...graphql.Option) (*T, error) {
+	var q T
+	return &q, client.Query(ctx, &q, vars, options...)
 }
 
 func run() error {
@@ -36,43 +80,8 @@ func run() error {
 	if user == "" {
 		return fmt.Errorf("user is required")
 	}
-
-	var (
-		prs         []*github.PullRequest
-		page        int
-		ctx         = context.Background()
-		client      = github.NewClient(nil).WithAuthToken(os.Getenv("GITHUB_TOKEN"))
-		owner, repo = ownerAndRepo(flag.Arg(0))
-	)
-	for {
-		list, res, err := client.PullRequests.List(ctx, owner, repo, &github.PullRequestListOptions{
-			State: state,
-			ListOptions: github.ListOptions{
-				Page:    page,
-				PerPage: 100,
-			},
-		})
-		if err != nil {
-			return err
-		}
-		prs = append(prs, list...)
-		if res.NextPage == 0 {
-			break
-		}
-		page = res.NextPage
-	}
-	var requested []*github.PullRequest
-	for _, pr := range prs {
-		for _, reviewer := range pr.RequestedReviewers {
-			if reviewer.GetLogin() == user {
-				requested = append(requested, pr)
-			}
-		}
-	}
-	for _, pr := range requested {
-		fmt.Printf("%s\n>  %s\n\n", pr.GetTitle(), pr.GetHTMLURL())
-	}
-	return nil
+	owner, repo := ownerAndRepo(flag.Arg(0))
+	return fetch(context.Background(), owner, repo, user)
 }
 
 func main() {
